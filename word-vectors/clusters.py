64b258ac83fc75dc3bb022e8e26f5f523fbcda4c
Sidharth Satapathy <sidharth.satapathy9@gmail.com>
2018-07-16 20:18:33 +0530

    Hcluster algorithm added.

--- word-vectors/clusters.py
+++ word-vectors/clusters.py
@@ -1,4 +1,14 @@
 from math import sqrt
+
+class bicluster:
+    def __init__(self,vec,left=None,right=None,distance=0.0,id=None):
+        self.left = left
+        self.right = right
+        self.vec = vec
+        self.id = id
+        self.distance = distance
+
+
 def readFile(filename):
     lines = [line for line in open(filename,'r')]
 
@@ -36,4 +46,43 @@ def pearson(v1,v2):
 
     if den == 0: return 0
 
-    return 1.0 - num/den    
+    return 1.0 - num/den
+
+
+# The hcluster algorithm
+
+def hcluster(row,distance=pearson):
+    distances={}
+    currentclustid=-1
+
+    # Clusters are initially just the rows
+    clust=[bicluster(row[i],id=i) for i in range(len(rows))]
+
+    while len(clust)>1:
+        lowestpair = (0,1)
+        closest = distance(clust[0].vec,clust[1].vec)
+
+        # looping through every pair looking for the smallest distance
+        for i in range(len(clust)):
+            for j in range(i+1,len(clust)):
+                # distances in the cache of distance calculations
+                if (clust[i].id,clust[j].id) not in distances:
+                    distances[(clust[i].id,clust[j].id)] = distance(clust[i].vec,clust[j].vec)
+                d = distances[(clust[i].id,clust[j].id)]
+
+                if d<closest:
+                    closest = d
+                    lowestpair=(i,j)
+
+        # calculating the average of the 2 clusters
+        mergevec = [(clust[lowestpair[0]].vec[i]+clust[lowestpair[1].vec[i]])/2.0 for i in range (len(clust[0].vec))]
+
+        # creating a new cluster
+        newcluster=bicluster(mergevec,left=clust[lowestpair[0]],right=clust[lowestpair[1]],distance=closest,id=currentclustid)                    
+
+        # clusted ids that weren't in the original set are negative
+        currentclustid-=1
+        del clust[lowestpair[1]]
+        del clust[lowestpair[0]]
+        clust.append(newcluster)
+    return clust[0]    
