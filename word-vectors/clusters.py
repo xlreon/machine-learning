2f9851de42a30ffe18ea0d07e6060b4e2d703b45
Sidharth Satapathy <sidharth.satapathy9@gmail.com>
2018-07-18 00:18:44 +0530

    Added k means cluster algo.

--- word-vectors/clusters.py
+++ word-vectors/clusters.py
@@ -1,5 +1,6 @@
 from math import sqrt
 from PIL import Image,ImageDraw
+import random
 
 class bicluster:
     def __init__(self,vec,left=None,right=None,distance=0.0,id=None):
@@ -168,4 +169,43 @@ def rotatematrix(data):
     for i in range(len(data[0])):
         newrow=[data[j][i] for j in range(len(data))]
         newdata.append(newrow)
-    return newdata            
+    return newdata 
+
+
+def kcluster(rows,distance=pearson,k=4):
+
+    # Determine the mimimum and max values for each point
+    ranges=[(min([row[i] for row in rows]), max([row[i]for row in rows])) for i in range(len(rows[0]))]
+
+    # Create k randomly placed centroids
+    clusters=[[random.random()*(ranges[i][1]-ranges[i][0])+ranges[i][0] for i in range(len(rows[0]))] for j in range(k)]
+
+    lastmatches=None
+    for t in range(100):
+        print ("Iteration %d" % t)
+        bestmatches=[[] for i in range(k)]
+
+        # Find which centroid is the closet for each row
+        for j in range(len(rows)):
+            row=rows[j]
+            bestmatch=0
+            for i in range(k):
+                d=distance(clusters[i],row)
+                if d < distance(clusters[bestmatch],row): bestmatch=i
+            bestmatches[bestmatch].append(j)
+
+            # If the results are the same as last time, this is complete
+            if bestmatches==lastmatches: break
+            lastmatches=bestmatches
+
+        # Moving the centroids to the average of their members
+            for i in range(k):
+                avgs=[0.0]*len(rows[0])
+                if len(bestmatches[i])>0:
+                    for rowid in bestmatches[i]:
+                        for m in range(len(rows[rowid])):
+                            avgs[m]+=rows[rowid][m]
+                    for j in range(len(avgs)):
+                        avgs[j]/=len(bestmatches[i])
+                    clusters[i]=avgs            
+        return bestmatches
